a5ff889a6fb1a95a32d37048dfcc189cfdc5ac4d
Javier Caicedo Tobon <javiercaicedotobon@gmail.com>
2024-08-17 09:50:00 -0500

    new update

--- citas/citas/settings/local.py
+++ citas/citas/settings/local.py
@@ -26,9 +26,11 @@ DATABASES = {
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATICFILES_DIRS = [BASE_DIR.child('static')]
 
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_ROOT = BASE_DIR.child('media')
 
 #configuracion para el envio de correos de verificacion
